c0bd84e321ef2787d64650b6fbb98706d2c7aa77
nyannos <nyanndz@gmail.com>
2026-09-22 08:00:33 +0700

    Add files via upload

--- nyann_os_redz-3.lua
+++ nyann_os_redz-3.lua
@@ -1366,7 +1366,7 @@ local Options = {}
 local Toggles = {}
 
 local RealWindow = redzlib:MakeWindow({
-	Title = "nyann os [Beta] Fram no key",
+	Title = "                  nyann os [Beta] No Key",
 	SubTitle = "by real_@nyannnokonoko",
 	SaveFolder = "Blox Fruits.lua",
 })
@@ -1604,7 +1604,7 @@ local Tabs = {
 	Main = Window:MakeTab({ Title = "Main", Icon = "box" }),
 	Font = Window:MakeTab({ Title = "Font", Icon = "type" }),
 	Settings = Window:MakeTab({ Title = "Setting", Icon = "settings" }),
-	Fish = Window:MakeTab({ Title = "Fishing", Icon = "rbxassetid://8826268749"}),
+	Fish = Window:MakeTab({ Title = "Fishing", Icon = "droplet"}),
 	Quests = Window:MakeTab({ Title = "Quest And Item", Icon = "scroll" }),
 	SeaEvent = Window:MakeTab({ Title = "Sea Event", Icon = "waves" }),
 	Race = Window:MakeTab({ Title = "Mirage And Race", Icon = "moon" }),
